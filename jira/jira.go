31fb3265ff8347b85b6fec0d63a923a558f3be09
Erdem Bozdag <erdem.bozdag@hootsuite.com>
2022-10-21 00:39:47 -0700

    Fix Jira mock

---
+++ jira/jira.go
@@ -0,0 +1,104 @@
+package jira
+
+import (
+	"go.uber.org/zap"
+	api "gopkg.in/andygrunwald/go-jira.v1"
+)
+
+// Issue is a Jira issue.
+type Issue struct {
+	ID      string
+	Key     string
+	Labels  []string
+	Summary string
+}
+
+// Configuration configures a Jira client.
+type Configuration struct {
+	BaseURL   string
+	BasicAuth BasicAuthConfiguration
+}
+
+// BasicAuthConfiguration configures basic authentication for Jira.
+type BasicAuthConfiguration struct {
+	User  string
+	Token string
+}
+
+//go:generate mockgen -destination=mocks/jira.go -package=mocks -source=jira.go
+type Jiraer interface {
+	Search(jql string) ([]Issue, error)
+}
+
+// Jira allows querying Jira issues.
+type Jira struct {
+	conf   Configuration
+	client *api.Client
+	logger *zap.Logger
+}
+
+// New creates a Jira or panics if it fails to.
+func NewOrFail(conf Configuration, logger *zap.Logger) *Jira {
+	jira, err := New(conf, logger)
+	if err != nil {
+		panic(err)
+	}
+	return jira
+}
+
+// New creates a new Jira.
+func New(conf Configuration, logger *zap.Logger) (*Jira, error) {
+
+	tp := api.BasicAuthTransport{
+		Username: conf.BasicAuth.User,
+		Password: conf.BasicAuth.Token,
+	}
+
+	client, err := api.NewClient(tp.Client(), conf.BaseURL)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Jira{
+		conf:   conf,
+		client: client,
+		logger: logger,
+	}, nil
+}
+
+// Search will query Jira API using the provided JQL string
+func (j *Jira) Search(jql string) ([]Issue, error) {
+
+	opt := &api.SearchOptions{
+		MaxResults: 50, // Set this arbitrarily high so we don't need to deal with pagination yet.
+		StartAt:    0,  // Make sure we start grabbing issues from last checkpoint
+	}
+
+	results, res, err := j.client.Issue.Search(jql, opt)
+	if err != nil || res == nil {
+		return nil, err
+	}
+
+	j.logger.Debug("found jira issues", zap.Any("jira.search.result", res))
+
+	// If we encounter this error it's time to implement pagination.
+	if res.Total > res.MaxResults {
+		j.logger.Error("jira search exceeded max results",
+			zap.String("jira.search.query", jql),
+			zap.Int("jira.search.total", res.Total),
+		)
+	}
+
+	var issues []Issue
+	for _, r := range results {
+		issues = append(issues, Issue{
+			Key:     r.Key,
+			ID:      r.ID,
+			Labels:  r.Fields.Labels,
+			Summary: r.Fields.Summary,
+		})
+	}
+
+	return issues, nil
+}
---
+++ jira/jira_test.go
@@ -0,0 +1,64 @@
+package jira
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	api "gopkg.in/andygrunwald/go-jira.v1"
+	"https://github.com/erdembozdg/erdem-go/jira/mocks"
+	"go.uber.org/zap"
+)
+
+func TestSearchJiras(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockJiraer := mocks.NewMockJiraer(ctrl)
+
+	i1 := api.Issue{
+		ID:  "111",
+		Key: "Test-1",
+		Fields: &api.IssueFields{
+			Summary: "Test-1: bla bla",
+			Labels:  []string{"fake1", "fake2"},
+		},
+	}
+
+	i2 := api.Issue{
+		ID:   "222",
+		Key:  "Test-2",
+		Self: "",
+		Fields: &api.IssueFields{
+			Summary: "Test-2: bla bla",
+			Labels:  []string{"fake1", "fake2"},
+		},
+	}
+
+	jql := "fake jql"
+	baseUrl := "https://fake.base"
+	jira := Jira{
+		conf: Configuration{
+			BaseURL: baseUrl,
+			BasicAuth: BasicAuthConfiguration{
+				User:  "ebzdag",
+				Token: "111",
+			},
+		},
+		client: mockJiraer,
+		logger: zap.NewNop(),
+	}
+
+	searchResult := []api.Issue{i1, i2}
+
+	mockJiraer.EXPECT().Search(jql).Times(1).Return(&searchResult, nil)
+	issues, err := jira.Search(jql)
+	assert.NoError(t, err)
+	assert.Len(t, issues, 2)
+	assert.Equal(t, i1.Key, issues[0].Key)
+	assert.Equal(t, i2.Key, issues[1].Key)
+	assert.Equal(t, i1.ID, issues[0].ID)
+	assert.Equal(t, i2.ID, issues[1].ID)
+	assert.Equal(t, i1.Fields.Summary, issues[0].Summary)
+	assert.Equal(t, i2.Fields.Summary, issues[1].Summary)
+	assert.Equal(t, []string{"fake1", "fake2"}, issues[0].Labels)
+	assert.Equal(t, []string{"fake1", "fake2"}, issues[1].Labels)
+}
